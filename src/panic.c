d5ff5ba6dfce062ade24cf5c60b2e42f9b7b2de8
Paweł Cholewa <MCPlayG@wp.pl>
2019-04-20 14:55:06 +0200

    Modified the kernel panic

--- src/panic.c
+++ src/panic.c
@@ -2,7 +2,8 @@
 
 void panic(char* text)
 {
-    /*TODO: implementation*/
+    os_print_string("A fatal error occurred and CMike had to shut down.\r\nYou need to restart your computer.\r\n\n");
+    os_print_string(text);
     while (1)
         __asm__ __volatile__("cli; hlt");
 }
