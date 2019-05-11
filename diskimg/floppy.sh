4ad1fe066cfc772eb07f2feab457f7c449e2ff75
Paweł Cholewa <MCPlayG@wp.pl>
2019-05-11 16:21:47 +0200

    Finished floppy generation script

--- diskimg/floppy.sh
+++ diskimg/floppy.sh
@@ -5,16 +5,20 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-rm $FLOPPY_IMG_PATH
-mkdosfs -C $FLOPPY_IMG_PATH 1440 || exit 1
-dd if=$PROJECT_ROOT_DIR/bootloader/bootloader.bin of=$FLOPPY_IMG_PATH bs=512 count=1 conv=notrunc || exit 1
+TMPDIR=/tmp/cmike_floppy_$RANDOM$RANDOM$RANDOM
+TMPIMG=$TMPDIR.flp
 
-rm -rf tmp
-mkdir tmp || exit 1
+dd if=/dev/zero of=$TMPIMG bs=512 count=2880 || exit 1
+dd if=$PROJECT_ROOT_DIR/bootloader/bootloader.bin of=$TMPIMG bs=512 count=1 conv=notrunc || exit 1
 
-mount -o loop -t vfat $FLOPPY_IMG_PATH tmp || exit 1
-cp $DISK_ROOT_DIRECTORY/** tmp || exit 1
+mkdir $TMPDIR || exit 1
 
-umount tmp || exit 1
-rm -rf tmp
+mount -o loop -t vfat $TMPIMG $TMPDIR || exit 1
+cp $DISK_ROOT_DIRECTORY/** $TMPDIR || exit 1
+
+umount $TMPDIR || exit 1
+rm -rf $TMPDIR
+
+cp $TMPIMG $FLOPPY_IMG_PATH
+
 exit 0
