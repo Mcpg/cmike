0df2087d709e4dd2489d049526ea3e3609644272
Paweł Cholewa <DaMcpg@protonmail.com>
2019-05-11 13:37:19 +0200

    Updates to the floppy image creation script

--- diskimg/floppy.sh
+++ diskimg/floppy.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# $DISK_ROOT_DIRECTORY
-# $FLOPPY_IMG_PATH
-
 if [ "$EUID" -ne 0 ]; then
     echo "ERROR: Only root can do that!"
     exit 1
 fi
 
-dd if=/dev/zero of=$FLOPPY_IMG_PATH bs=512 count=2880
-dd if=$PROJECT_ROOT_DIR/bootloader/bootloader.bin of=$FLOPPY_IMG_PATH bs=512 count=1 conv=notrunc
+rm $FLOPPY_IMG_PATH
+mkdosfs -C $FLOPPY_IMG_PATH 1440 || exit 1
+dd if=$PROJECT_ROOT_DIR/bootloader/bootloader.bin of=$FLOPPY_IMG_PATH bs=512 count=1 conv=notrunc || exit 1
 
 rm -rf tmp
-mkdir tmp
+mkdir tmp || exit 1
 
-mount $FLOPPY_IMG_PATH tmp
-cp $DISK_ROOT_DIRECTORY/** tmp
+mount -o loop -t vfat $FLOPPY_IMG_PATH tmp || exit 1
+cp $DISK_ROOT_DIRECTORY/** tmp || exit 1
 
-umount tmp
+umount tmp || exit 1
 rm -rf tmp
 exit 0
