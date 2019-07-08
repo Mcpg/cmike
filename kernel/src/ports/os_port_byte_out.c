d90f959550a9e2d2f42e79e145b367747d132bd8
Paweł Cholewa <MCPlayG@wp.pl>
2019-07-08 19:56:44 +0200

    Implement os_port_byte_out

--- kernel/src/ports/os_port_byte_out.c
+++ kernel/src/ports/os_port_byte_out.c
@@ -7,5 +7,6 @@ void _os_port_byte_out(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 
 void os_port_byte_out(int address, char byte)
 {
-    
+    /* Pretty much a wrapper around outb */
+    asm volatile("outb %0, %1" : : "a" (byte), "Nd" (address));
 }
