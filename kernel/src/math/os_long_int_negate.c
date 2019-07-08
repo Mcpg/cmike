222526359ddd698c52efa71d17ffa560f94d1b74
Paweł Cholewa <MCPlayG@wp.pl>
2019-07-08 20:26:05 +0200

    Implement os_long_int_negate

--- kernel/src/math/os_long_int_negate.c
+++ kernel/src/math/os_long_int_negate.c
@@ -7,5 +7,8 @@ void _os_long_int_negate(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 
 void os_long_int_negate(int* hi, int* lo)
 {
-    /* TODO */
+    long temp = (*lo) | ((*hi) << 16);
+    temp = -temp;
+    *lo = temp & 0x0000FFFF;
+    *hi = (temp >> 16) & 0x0000FFFF;
 }
