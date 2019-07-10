59e456f893358f4b244ea20ab1aa6124cba19e4b
Paweł Cholewa <MCPlayG@wp.pl>
2019-07-10 12:49:39 +0200

    Implement sound functions

--- kernel/src/sound/os_speaker_off.c
+++ kernel/src/sound/os_speaker_off.c
@@ -7,5 +7,5 @@ void _os_speaker_off(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 
 void os_speaker_off()
 {
-    
+    os_port_byte_out(0x61, os_port_byte_in(0x61) & 0xFC);
 }
--- kernel/src/sound/os_speaker_tone.c
+++ kernel/src/sound/os_speaker_tone.c
@@ -7,5 +7,8 @@ void _os_speaker_tone(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 
 void os_speaker_tone(int frequency)
 {
-    
+    os_port_byte_out(0x43, 0xB6);
+    os_port_byte_out(0x42, frequency & 0x00FF);
+    os_port_byte_out(0x42, (frequency >> 8) & 0x00FF);
+    os_port_byte_out(0x61, (os_port_byte_in(0x61) | 0x03) & 0xFF);
 }
