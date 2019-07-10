14ad6ac289c7db44f9fceef954f084005f5a95b6
Paweł Cholewa <MCPlayG@wp.pl>
2019-07-10 12:38:05 +0200

    Fix os_print_string ...and don't bring the whole kernel down with faulty prints

--- kernel/src/screen/os_print_string.c
+++ kernel/src/screen/os_print_string.c
@@ -10,18 +10,15 @@ void _os_print_string(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 
 void os_print_string(char* string, char color)
 {
-    char row1, col1;
-    char row2, col2;
-
     for (int i = 0; string[i]; i++)
     {
-        if ((string[i] < ' ' || string[i] == 127) && string[i] > 0)
+        /* Check if the character is printable */
+        if (string[i] >= 0 && string[i] < 32)
         {
-            asm("int $0x10" : : "a" (0x0E00 | (unsigned char) string[i]));
+            asm volatile("int $0x10" : : "a" (0x0E00 | string[i]));
             continue;
         }
-        asm volatile("int $0x10" : : "a" (0x0900 | (unsigned char) string[i]), "b" (0x0000 | color & 0x00FF), "c" (1));
+        asm volatile("int $0x10" : : "a" (0x0900 | (unsigned char) string[i]), "b" (0x0000 | (color & 0x00FF)), "c" (1));
         asm volatile("int $0x10" : : "a" (0x0E00 | (unsigned char) string[i]));
-        //os_wait_for_key();
     }
 }
