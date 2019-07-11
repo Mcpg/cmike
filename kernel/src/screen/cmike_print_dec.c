ad4f0c27b3eecc5ec45d0b6e2e1bd70fbb1472f9
Paweł Cholewa <DaMcpg@protonmail.com>
2019-07-12 00:23:58 +0200

    Implement negative value suppot to cmike_print_dec

--- kernel/src/screen/cmike_print_dec.c
+++ kernel/src/screen/cmike_print_dec.c
@@ -2,9 +2,16 @@
 
 void cmike_print_dec(int number, char color)
 {
+    char negative = 0;
     int i = 0;
     char* buffer = "00000";
     
+    if (number < 0)
+    {
+        negative = 1;
+        number = -number;
+    }
+
     if (number == 0)
     {
         cmike_print_char(digits[0], color);
@@ -15,6 +22,11 @@ void cmike_print_dec(int number, char color)
     {
         buffer[i] = digits[number % 10];
         number /= 10;
+    }
+
+    if (negative)
+    {
+        cmike_print_char('-', color);
     }
 
     buffer += i + 1;
