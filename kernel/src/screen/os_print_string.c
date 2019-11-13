dc64f2ad774fb72f7bd92b36e9a32b0393bcde5c
Paweł Cholewa <DaMcpg@protonmail.com>
2019-11-13 21:45:10 +0100

    os_print_string: allow attribute inheritance  * If 0 is passed as the color in os_print_string,    the attribute byte will not be updated.
       That's how several MikeOS programs generate color,
       depsite having no actual "print colored string"
       syscall in the kernel.

--- kernel/src/screen/os_print_string.c
+++ kernel/src/screen/os_print_string.c
@@ -7,9 +7,11 @@
 
 void _os_print_string(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 {
-    os_print_string((char*) *si, DEFAULT_COLOR);
+    os_print_string((char*) *si, 0x00);
 }
 
+/* If passed color is 0x00, no color will actually be applied,
+   and the text will inherit attribute of a given text cell */
 void os_print_string(char* string, char color)
 {
 #ifdef OS_PRINT_STRING_DEBUG
@@ -19,7 +21,7 @@ void os_print_string(char* string, char color)
     for (int i = 0; string[i]; i++)
     {
         /* Check if the character is printable */
-        if (string[i] >= 0 && string[i] < 32)
+        if (string[i] >= 0 && string[i] < 32 || color == 0x00)
         {
             asm volatile("int $0x10" : : "a" (0x0E00 | string[i]));
             continue;
