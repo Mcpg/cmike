088fd4381d894d27abdf2bfabac5f549d2f2805f
Paweł Cholewa <DaMcpg@protonmail.com>
2019-11-13 21:32:04 +0100

    Fix os_move_cursor Assembly handler

--- kernel/src/screen/os_move_cursor.c
+++ kernel/src/screen/os_move_cursor.c
@@ -2,7 +2,7 @@
 
 void _os_move_cursor(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 {
-    os_move_cursor(*dx & 0xFF00, *dx & 0x00FF);
+    os_move_cursor((*dx & 0xFF00) >> 8, *dx & 0x00FF);
 }
 
 void os_move_cursor(char row, char column)
