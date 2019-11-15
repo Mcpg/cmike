f31004b3dcda9ff9efce748a745a2a8577a374e9
Paweł Cholewa <DaMcpg@protonmail.com>
2019-11-15 14:18:29 +0100

    Implement os_string_tokenize

--- kernel/src/string/os_string_tokenize.c
+++ kernel/src/string/os_string_tokenize.c
@@ -2,10 +2,25 @@
 
 void _os_string_tokenize(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 {
-    *di = os_string_tokenize(*ax & 0x00FF, (char*) *si);
+    *di = (int) os_string_tokenize(*ax & 0x00FF, (char*) *si);
 }
 
-int os_string_tokenize(char separator, char* beginning)
+/*
+ * This function changes first occurance of character `separator` in string
+ * pointed by parameter `string` and replaces it to 0. Then, it returns
+ * index of the changed character + 1, or NULL if separator wasn't
+ * found.
+ */
+char* os_string_tokenize(char separator, char* string)
 {
-    return 0;
+	while (*string != NULL)
+	{
+		if (*string == separator)
+		{
+			*string = NULL;
+			return string + 1;
+		}
+		string++;
+	}
+    return NULL;
 }
