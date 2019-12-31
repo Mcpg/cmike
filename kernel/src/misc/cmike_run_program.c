4f88e7411d6569524a1adc9cb8466ee74621422f
Paweł Cholewa <DaMcpg@protonmail.com>
2019-12-31 18:54:38 +0100

    cmike_run_program: call the program with registers zeroed out

--- kernel/src/misc/cmike_run_program.c
+++ kernel/src/misc/cmike_run_program.c
@@ -4,6 +4,9 @@ void cmike_run_program(char* path)
 {
     memset(program_space, 0, 32767);
     os_load_file(path, program_space);
-    ((void (*)()) program_space)();
+    
+    /* TODO: command line parameter support */
+    asm volatile("call 0x8000" : : "a"(0),"b"(0),"c"(0),"d"(0),"S"(0),"D"(0));
+
     os_print_newline(DEFAULT_COLOR);
 }
