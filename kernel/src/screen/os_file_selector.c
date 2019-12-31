3f1831cbcb31977d7a9e0bbee7080c82dc7ad45f
Paweł Cholewa <DaMcpg@protonmail.com>
2019-12-31 21:16:19 +0100

    kernel: implement os_file_selector

--- kernel/src/screen/os_file_selector.c
+++ kernel/src/screen/os_file_selector.c
@@ -2,7 +2,9 @@
 
 void _os_file_selector(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
 {
-    if (os_file_selector((char*) *ax))
+    *ax = (int) os_file_selector();
+
+    if ((*ax) != 0)
     {
         __asm__ __volatile__("clc");
     }
@@ -12,7 +14,40 @@ void _os_file_selector(int* ax, int* bx, int* cx, int* dx, int* si, int* di)
     }
 }
 
-int os_file_selector(char* file)
+char* os_file_selector()
 {
-    return 0;
+    static char filename[12] = { 0 };
+
+    char buffer[1024] = { 0 };
+    os_get_file_list(buffer);
+
+    int option = os_list_dialog(buffer, "Pick a file, or press [Esc] key", "_");
+
+    if (option == 0)
+        return NULL;
+
+    memset(filename, 0, sizeof(filename));
+
+    option--;
+
+    /* Find beginning of selected option in the buffer... */
+    char* current = buffer;
+    while (option > 0)
+    {
+        if (*current == ',')
+            option--;
+        current++;
+    }
+
+    /* ...and then copy it to filename buffer */
+    char* current_filename = filename;
+    while (*current != ',' && *current != 0)
+    {
+        *current_filename = *current;
+        current++;
+        current_filename++;
+    }
+    *current_filename = 0;
+
+    return filename;
 }
